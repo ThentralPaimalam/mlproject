3f46a0945f843f5f2281d7804375f9e8f567be53
ThentralParimalam <thentral.000.s@gmail.com>
2025-04-30 00:31:03 +0530

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import sys
 import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
-
+import dill
 from src.exception import CustomException
 
 def save_object(file_path, obj):
